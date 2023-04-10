ce41ddf77018f6e358db996758e0ab2d2095ab9d
Carlos <carloshlzansavio@gmail.com>
2023-04-10 00:12:32 -0300

    Add smtp notification

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module poliedros/arthur-worker
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-mail/mail v2.3.1+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
--- main.go
+++ main.go
@@ -4,9 +4,42 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+
+	"github.com/go-mail/mail"
 
 	"github.com/joho/godotenv"
 )
+
+func notify() {
+	m := mail.NewMessage()
+
+	m.SetHeader("From", os.Getenv("FROM"))
+
+	m.SetHeader("To", os.Getenv("TO"))
+
+	url := os.Getenv("URL")
+	title := fmt.Sprintf("%s OFF", url)
+	m.SetHeader("Subject", title)
+
+	body := fmt.Sprintf("Check <a href='%s'>%s</a> now", url, url)
+	m.SetBody("text/html", body)
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		fmt.Println("Port casting error.")
+	}
+
+	d := mail.NewDialer(os.Getenv("SMTP_HOST"), port, os.Getenv("FROM"), os.Getenv("PASSWORD"))
+
+	if err := d.DialAndSend(m); err != nil {
+
+		panic(err)
+
+	}
+
+	fmt.Println("Email sent.")
+}
 
 func main() {
 	err := godotenv.Load(".env")
@@ -21,6 +54,7 @@ func main() {
 
 	if err != nil {
 		fmt.Println("Error: ", err)
+		notify()
 		return
 	}
 	defer res.Body.Close()
